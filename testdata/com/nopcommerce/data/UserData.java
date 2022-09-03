d5b2e46a818ba07d78e73ec7c74b93a2ad3997eb
LeThanhLoc <lethanhloc89@gmail.com>
2022-09-03 15:31:11 +0700

    Init Automation Framework

---
+++ testdata/com/nopcommerce/data/UserData.java
@@ -0,0 +1,19 @@
+package com.nopcommerce.data;
+
+public class UserData {
+	public void Register() {
+
+	}
+
+	public void Address() {
+
+	}
+
+	public void Login() {
+
+	}
+
+	public void MyAccount() {
+
+	}
+}
