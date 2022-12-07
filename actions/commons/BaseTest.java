99d8d53f23b22369453c775bedc02339861c7c00
LeThanhLoc <lethanhloc89@gmail.com>
2022-12-07 22:35:32 +0700

    Close Browser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,6 +1,7 @@
 package commons;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
@@ -20,9 +21,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	private WebDriver driver;
-	
+
 	protected final Log log;
-	
+
 	protected BaseTest() {
 		log = LogFactory.getLog(getClass());
 	}
@@ -31,23 +32,23 @@ public class BaseTest {
 	public void initBeforeSuite() {
 		deleteAllureFolder();
 	}
-	
+
 	public void deleteAllureFolder() {
 		try {
 			String pathFolderDownload = GlobalConstants.PROJECT_PATH + "/allure-json";
 			File file = new File(pathFolderDownload);
 			File[] listOfFiles = file.listFiles();
-			for (int i=0;i<listOfFiles.length;i++) {
-				if(listOfFiles[i].isFile()) {
+			for (int i = 0; i < listOfFiles.length; i++) {
+				if (listOfFiles[i].isFile()) {
 					System.out.println(listOfFiles[i].getName());
 					new File(listOfFiles[i].toString()).delete();
 				}
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.out.print(e.getMessage());
 		}
 	}
-	
+
 	protected WebDriver getBrowserDriver(String browserName) {
 		BrowserList browserList = BrowserList.valueOf(browserName.toUpperCase());
 
@@ -177,8 +178,69 @@ public class BaseTest {
 		}
 		return pass;
 	}
-	
+
 	public WebDriver getDriver() {
 		return this.driver;
+	}
+
+	protected void closeBrowserAndDriver() {
+		String cmd = "";
+		try {
+			String osName = System.getProperty("os.name").toLowerCase();
+			log.info("OS name = " + osName);
+
+			String driverInstanceName = driver.toString().toLowerCase();
+			log.info("Driver instance name = " + driverInstanceName);
+
+			if (driverInstanceName.contains("chrome")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq chromedriver*\"";
+				} else {
+					cmd = "pkill chromedriver";
+				}
+			} else if (driverInstanceName.contains("internetexplorer")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq IEDriverServer*\"";
+				}
+			} else if (driverInstanceName.contains("firefox")) {
+				if (osName.contains("windows")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq geckodriver*\"";
+				} else {
+					cmd = "pkill geckodriver";
+				}
+			} else if (driverInstanceName.contains("edge")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq msedgedriver*\"";
+				} else {
+					cmd = "pkill msedgedriver";
+				}
+			} else if (driverInstanceName.contains("opera")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq operadriver*\"";
+				} else {
+					cmd = "pkill operadriver";
+				}
+			} else if (driverInstanceName.contains("safari")) {
+				if (osName.contains("mac")) {
+					cmd = "pkill safaridriver";
+				}
+			}
+
+			if (driver != null) {
+				driver.manage().deleteAllCookies();
+				driver.quit();
+			}
+		} catch (Exception e) {
+			log.info(e.getMessage());
+		} finally {
+			try {
+				Process process = Runtime.getRuntime().exec(cmd);
+				process.waitFor();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
---
+++ testcases/com/nopcommerce/user/Level_17_Close_Browser.java
@@ -0,0 +1,79 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+
+import com.nopcommerce.common.Common_01_Register_New_Account;
+
+import commons.BaseTest;
+import pageObjects.nopCommer.user.UserHomePageObject;
+import pageObjects.nopCommer.user.UserLoginPageObject;
+import pageObjects.nopCommer.user.PageGeneratorManager;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+
+public class Level_17_Close_Browser extends BaseTest {
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		driver = getBrowserDriver(browserName);
+		homePage = PageGeneratorManager.getUserHomePage(driver);
+		
+		emailAddress = Common_01_Register_New_Account.emailAddress;
+		password = Common_01_Register_New_Account.password;
+
+		loginPage = homePage.openUserLoginPage();
+
+		loginPage.InputToEmailTextbox(emailAddress);
+
+		loginPage.InputToPasswordTextbox(password);
+
+		loginPage.clickToLoginButton();
+	}
+
+	@Test
+	public void Search__01_Empty_Data() {
+
+	}
+	
+	
+	public void Search__02_Relative_Product_Name() {
+		
+	}
+	
+	public void Search__03_Absoulute_Product_Name() {
+		
+	}
+	
+	public void Search__04_Paretn_Category() {
+		
+	}
+	
+	public void Search__05_Incorrect_Manufactorer() {
+		
+	}
+	
+	public void Search__06_Correct_Manufactorer() {
+		
+	}
+	@AfterClass
+	public void afterClass() {
+		closeBrowserAndDriver();
+	}
+
+	public int generateFakeNumber() {
+		Random rad = new Random();
+		return rad.nextInt(999);
+	}
+
+	private UserHomePageObject homePage;
+	private UserLoginPageObject loginPage;
+	private WebDriver driver;
+	private String emailAddress,password;
+}
